fd87511d670a8a67fcc5fb530f6fbea613586c7f
Noah Cristino <noahcristino@yahoo.com>
2016-11-20 09:49:30 -0500

    Uploaded Python File

---
+++ busfind.py
@@ -0,0 +1,12 @@
+import urllib2
+import xml.etree.ElementTree
+print "MA Bus Tracker"
+apikey = "wX9NwuHnZU2ToO7GmGR9uw"
+lat = raw_input("Latitude: ")#"42.346961"
+lon = raw_input("Longitude: ")#"-71.076640"
+url = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key="+apikey+"&lat="+str(lat)+"&lon="+str(lon)+"&format=xml"
+f = urllib2.urlopen(url)
+data = f.read()
+e = xml.etree.ElementTree.fromstring(data)
+for atype in e.findall('stop'):
+    print(atype.get('stop_name') + " | " + atype.get('stop_lat')+", "+atype.get('stop_lon') + " | " + atype.get('distance')+" miles")
